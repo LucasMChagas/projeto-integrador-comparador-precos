fa91418313f36bf464f22bd67b51fb1d9bc27832
Lucas Miranda <lucas_miranda_chagas@hotmail.com>
2025-05-09 08:50:10 -0300

    fix: alteração do caminho do produtos.json

--- src/interfaces/api/search_router.py
+++ src/interfaces/api/search_router.py
@@ -5,7 +5,7 @@ from services.search_service import buscar_produtos
 
 search_router = APIRouter(prefix="/products", tags=["Products"])
 
-@search_router.get("/produtos", response_model=List[dict])
+@search_router.get("/", response_model=List[dict])
 def search(nome: str = Query(..., description="Nome do produto para buscar")):
     list = buscar_produtos(nome)
     return list
--- src/services/search_service.py
+++ src/services/search_service.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI, Query
 from typing import List
 import json
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(BASE_DIR,"..", "data", "produtos.json")
 
 def buscar_produtos(nome: str):
-    with open("/data/produtos.json", "r", encoding="utf-8") as f:
+    with open(file_path, "r", encoding="utf-8") as f:
         produtos = json.load(f)
 
     resultados = [
